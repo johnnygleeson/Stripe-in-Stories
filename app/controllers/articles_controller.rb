01b98c79c6b15ca12792212a5dc2525c8a7fba87
John Gleeson <johnnygleeson@gmail.com>
2015-09-15 08:17:42 +0100

    installed simpleform and created article show pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
 
 	def index
 	end
+
+	def show
+	end
+	 	
 
 	def new
 		@article = Article.new
@@ -14,9 +19,16 @@ class ArticlesController < ApplicationController
 	    else
 	    render 'new'
 	 end
+
+	 
+
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params 
 	 params.require(:article).permit(:title, :content)
